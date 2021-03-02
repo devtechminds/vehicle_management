67acc57a77be0d2f7ab1725dcc173cce05b8525e
sanibr <sanibr89@gmail.com>
2021-03-02 22:41:12 +0530

    loading and unloading gate entrie creation

---
+++ app/Http/Controllers/LuGateEntrieController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\LuGateEntrie;
+use Illuminate\Http\Request;
+
+class LuGateEntrieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\LuGateEntrie  $luGateEntrie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LuGateEntrie $luGateEntrie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\LuGateEntrie  $luGateEntrie
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LuGateEntrie $luGateEntrie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\LuGateEntrie  $luGateEntrie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LuGateEntrie $luGateEntrie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\LuGateEntrie  $luGateEntrie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LuGateEntrie $luGateEntrie)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_03_02_151335_create_lu_gate_entries_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLuGateEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lu_gate_entries', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->id();
+            $table->text('ref_no');
+            $table->date('date');
+            $table->Integer('customer_name');
+            $table->Integer('commodity');
+            $table->Integer('transporter');
+            $table->String('truck_no')->nullable();
+            $table->String('trailer_no')->nullable();
+            $table->String('dn_no')->nullable();
+            $table->Integer('dn_qty')->nullable();
+            $table->String('bl_no')->nullable();
+            $table->Integer('bl_qty')->nullable();
+            $table->Integer('quantity')->nullable();
+            $table->decimal('metric_ton', 8, 5)->nullable();
+            $table->String('driver_name')->nullable();
+            $table->String('driver_lic_no')->nullable();
+            $table->String('driver_ph_no')->nullable();
+            $table->String('interchange_no');
+            $table->String('destination');
+            $table->String('container_no')->nullable();
+            $table->String('shipping_line');
+            $table->String('tra_seal_no');
+            $table->String('gate_pass_no')->nullable();
+            $table->String('time_in');
+            $table->boolean('status')->default('0');
+            $table->Integer('created_by');
+            $table->Integer('updated_by');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lu_gate_entries');
+    }
+}
